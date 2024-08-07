b03e5ce4e157f6a25e0000d3618366d3240c7097
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-08 01:07:16 +0300

    js selectors added

---
+++ app.js
@@ -0,0 +1,13 @@
+//?HARCAMA FORMU
+const tarihInput = document.querySelector("#date");
+const miktarInput = document.querySelector("#price");
+const harcamaAlani = document.querySelector("priceArea");
+
+//?GELİR FORMU
+const gelirInput = document.querySelector("#gelir");
+
+//? TABLO ALANI
+const infoTable = document.querySelector(".infoTable");
+
+//? BUTONLAR
+const tumunuTemizle = document.querySelector(".btn-dark");
