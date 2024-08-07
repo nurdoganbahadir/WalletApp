88b6bfb1bd8f35a8f7b9ddfd64f9c4d61f911bcc
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-08 01:12:14 +0300

    selectors update

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 const tarihInput = document.querySelector("#date");
 const miktarInput = document.querySelector("#price");
 const harcamaAlani = document.querySelector("priceArea");
+const saveBtn = document.querySelector(".btn-danger");
 
 //?GELİR FORMU
 const gelirInput = document.querySelector("#gelir");
+const gelirBtn = document.querySelector(".btn-primary");
 
 //? TABLO ALANI
 const infoTable = document.querySelector(".infoTable");
 
 //? BUTONLAR
 const tumunuTemizle = document.querySelector(".btn-dark");
+
+//İLK OLARAK BOŞ LİSTE AÇILACAK VE GELİR İÇİN BİR TOPLAMA İNTEGERI OLUŞTURULACAK
+
+let harcamaListesi = [];
+let tumGelirler = 0;
+
+//? Locale storage kullanılacağı için butonlar submit edicek
+
+document.querySelector();
